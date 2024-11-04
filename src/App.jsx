014bf5b3ac0749bb5e4c8887f9a04a090bd82eed
jhamayank31 <mayankjha162@gmail.com>
2024-11-04 17:23:09 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar"
+import NewsBoard from "./components/NewsBoard"
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsBoard/>
+    </div>
+  )
+}
+export default App
